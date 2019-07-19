56f2c1f9f3123092e0b23b06740098b0ce6bc112
BrandonSYu <beyu94@gmail.com>
2019-07-19 16:12:17 -0700

    Docker EC2 html template, server endpoints complete

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const proxy = require('express-http-proxy');
 var express  = require('express');
-var app      = express();
+var app  = express();
 //var httpProxy = require('http-proxy');
 //var apiProxy = httpProxy.createProxyServer();
 app.use(express.static(__dirname + '/../'));
 
 const port = 5000;
-var serverOne = 'http://localhost:3001',
-    ServerTwo = 'http://localhost:3002',
-    ServerThree = 'http://localhost:3003',
-    ServerFour = 'http://localhost:3004'
+var serverOne = 'http://ec2-52-90-9-19.compute-1.amazonaws.com/',
+    ServerTwo = 'http://18.191.38.179/',
+    ServerThree = 'http://13.52.99.182:1337',
+    ServerFour = ''
  
 // app.all("/photos", function(req, res) {
 //     console.log('redirecting to Server1 Photos');
@@ -31,9 +31,39 @@ var serverOne = 'http://localhost:3001',
 //     apiProxy.web(req, res, {target: ServerFour});
 // });
 
-app.use('/header', proxy(ServerTwo));
-app.use('/photos', proxy(serverOne));
-app.use('/reviews', proxy(ServerThree));
+// app.use('/header', proxy(ServerTwo));
+// app.use('/photos', proxy(serverOne));
+// app.use('/reviews', proxy(ServerThree));
+
+
+app.use('/header', proxy(ServerTwo, {
+    proxyReqPathResolver: req => {
+      if(req.url === '/bundle.js') {
+        return req.url;
+      } else {
+        return '/header' + req.url
+      }
+    }
+  }));;
+app.use('/reviews', proxy(ServerThree, {
+    proxyReqPathResolver: req => {
+      if(req.url === '/dist/bundle.js') {
+        return req.url;
+      } else {
+        return '/reviews' + req.url
+      }
+    }
+  }));
+app.use('/photos', proxy(serverOne, {
+    proxyReqPathResolver: req => {
+      if(req.url === '/main.js') {
+        return req.url;
+      } else {
+        return '/photos' + req.url
+      }
+    }
+  }));
+  ;
 
 app.get('/', (req, res)=>{
     res.end();
